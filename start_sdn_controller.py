5b6592ce1d3c208753ad8442d91f8a5f9e7a5c3a
Samu902 <samuelemarrani@gmail.com>
2026-05-15 16:19:02 +0000

    fix script start controller

--- start_sdn_controller.py
+++ start_sdn_controller.py
@@ -1,4 +1,11 @@
 from ryu.cmd import manager
+import sys
 
 if __name__ == "__main__":
+    sys.argv = [
+        "ryu-manager",
+        "--verbose",
+        "controller.ryu_controller"
+    ]
+
     manager.main()
